a1e7ba91557ece50bf096c752253f5e1b0299ad8
Daedra <foglzerika@gmail.com>
2020-07-21 22:52:00 -0500

    feat | Create tsconfig file and configure it

---
+++ dist/helloWorld.js
@@ -0,0 +1,5 @@
+"use strict";
+var greet = "Hello my fist program with typescript";
+var nameUser = "My name is Daedra";
+console.log(nameUser);
+console.log(greet);
---
+++ src/helloWorld.ts
@@ -0,0 +1,4 @@
+const greet: string = "Hello my fist program with typescript";
+const nameUser: string ="My name is Daedra";
+console.log(nameUser)
+console.log(greet)
